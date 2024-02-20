59b6e9517bd2f939bac9188180d05279d9b7c57f
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-02-20 13:32:04 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 	printf("%d ", n);
 	if (n > 0)
 	{
-		printf("is positive \n");
+		printf("is positive\n");
 	}
 	else if (n < 0)
 	{
-		printf("is negative \n");
+		printf("is negative\n");
 	}
 	else
 	{
-		printf("is zero \n");
+		printf("is zero\n");
 	}
 	return (0);
 }
