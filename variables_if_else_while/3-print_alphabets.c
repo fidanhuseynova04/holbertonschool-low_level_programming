e6c1086671c8cc68fb840e95dfb497faddaf18ce
fidanhuseynova04 <fidanhsynv04@.com>
2024-02-21 12:23:09 +0000

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char a = 'a';
+	char b = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
