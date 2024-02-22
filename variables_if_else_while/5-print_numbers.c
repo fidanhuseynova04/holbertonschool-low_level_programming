21e6e39506903740146d93d929654f056492722e
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-02-22 19:19:23 +0000

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,18 +2,17 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int a;
-	char b = '0';
+	char a;
 
-	for (a = 0; a < 10 ; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		putchar(b)
-		b++;
+		putchar(a)
 	}
 	putchar('\n');
 	return (0);
