4c26a55aaeabf7855ab9d3a9996d1d51d109a904
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-02-23 07:03:14 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[] = "_putchar";
+	int b = strlen(a);
+	int i;
+
+	for (i = 0; i < b; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
