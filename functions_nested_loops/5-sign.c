e15e60d23d680a048463b2b8c053f5132e710189
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-02-23 21:04:31 +0000

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - check lowercase.
+ * @n: variable
+ * Return: if positive 1, zero 0, negative -1.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
