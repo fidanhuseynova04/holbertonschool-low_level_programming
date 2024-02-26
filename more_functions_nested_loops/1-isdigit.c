81be2314a1e628ed4c5c5806a163900cf23211c4
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-02-26 13:19:45 +0000

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 
 /**
  * _isdigit - check the code.
- * c@: valiable
+ * c@: variable
  * Return: 1 or 0
  */
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57 )
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
