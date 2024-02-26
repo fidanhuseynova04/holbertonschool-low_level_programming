540161b60fa6c725a161821a988bea71ab89b17f
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-02-26 13:54:04 +0000

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - check the code.
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
