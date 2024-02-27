d24deff734ef6750b9e604c243a112944746a5c0
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-02-27 14:00:03 +0000

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,9 +9,9 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 1; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			_putchar(' ');
 		}
