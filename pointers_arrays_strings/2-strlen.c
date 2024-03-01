b1b3c02f1bfc4be790dede3782620ef965e3a084
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-03-01 20:02:14 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code
+ * @s: variable
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int length = strlen(str);
+
+	printf("%d", length);
+
+	return (0);
+}
