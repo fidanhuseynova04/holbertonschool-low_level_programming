0cad0b3e6fda8a801819558f845067feab243fc3
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-03-16 21:05:55 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = j;
 	}
-	str = malloc(sizeof(char) * (i + j) + 1);
+	str = malloc(sizeof(char) * (i + n) + 1);
 	if (str == NULL)
 	{
 		return (NULL);
