f9e9bd962588a5a233cf0740888431a25287293c
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-03-21 10:38:55 +0000

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * init_dog - check the code
+ * @d: struct dog
+ * @name: name of dog
+ * @age: age of th dog
+ * @owner: owner of the dog
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
