86991049e340577f4baf1fb5b979b8700ee8111f
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-03-22 10:38:19 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,9 +11,8 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *dog = malloc(sizeof(dog_t));
 
-	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 	{
 		return (NULL);
