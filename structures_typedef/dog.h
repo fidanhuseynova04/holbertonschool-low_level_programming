869bc69e7707d334927df6744b4d39c6d1947f75
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-03-22 12:37:58 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,9 +13,9 @@ struct dog
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 void *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
