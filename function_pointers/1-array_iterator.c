3a43b986196a1f82067f7a6e9274e1ffcfe4ed61
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-03-22 20:08:47 +0000

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_iterator - prints an integer
+ * @array: array
+ * @size: size
+ * @action: pointer func
+ * Return: Always 0.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action =! NULL || size =! 0 || array =! NULL)
+	{
+	for (i = 0; i < size; i++)
+		(*action)(*(array + (int)i));
+	}
+}
