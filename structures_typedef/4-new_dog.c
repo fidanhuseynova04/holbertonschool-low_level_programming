aab23e539acf876e1eb638b3c8a053139a79d978
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-03-22 11:36:45 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * new_dog - check the code
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  * Return: Always 0.
  */
 dog_t *new_dog(char *name, float age, char *owner)
