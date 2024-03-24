e41a10c4f8d25e89bbb8aca9df2463a827041418
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-03-24 16:13:04 +0000

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,48 +3,46 @@
 #include <stdarg.h>
 #include <string.h>
 /**
- * print_all - print arguments
+ * print_all - prints all
  * @format: format
- *
  * Return: Always 0.
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
 	va_list args;
+	int i = 0, k = 0;
 	char *s;
 
 	va_start(args, format);
-
-	while (format && format[i])
+	if (format)
 	{
-		switch (format[i++])
+		while (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				s = va_arg(args, char*);
-				if (!s)
-				{
-					s = "(nil)";
-				}
-				printf("%s", s);
-				break;
-			default:
-				i++;
-				continue;
+			k = 1;
+			i++;
+			switch (format[i - 1])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				case 's':
+					s = va_arg(args, char*);
+					if (!s)
+						s = "(nil)";
+					printf("%s", s);
+					break;
+				default:
+					continue;
+			}
+			while (k && format[i])
+				printf(", "), k = 0;
 		}
-		if (format[i + 1] != '\0')
-			printf(", ");
-
-		i++;
 	}
 	printf("\n");
 	va_end(args);
