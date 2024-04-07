a27469251a2080138c61540ac81c17bfbe44a4a3
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-04-07 10:46:35 +0000

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * print_list- check the code
+ * @h: head
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h);
+{
+	size_t node = 0;
+
+	while (h)
+	
