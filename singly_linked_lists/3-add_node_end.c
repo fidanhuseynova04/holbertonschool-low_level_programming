a9d4a18c2ec87a079826800d6177998d01bbf0e5
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-04-07 15:35:19 +0000

    3-add_node_end.c

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node_end - check the code
+ * @head: head
+ * @str: string
+ * Return: Always 0.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_n = malloc(sizeof(list_t));
+	list_t *last;
+
+	if (new_n == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new_n;
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_n;
+	}
+	return (new_n);
+}
