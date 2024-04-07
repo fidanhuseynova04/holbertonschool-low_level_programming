d4d4c31adca2b6cf6dc8cf789b6bb51267c727aa
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-04-07 17:06:42 +0000

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,29 +11,31 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	int lens = 0;
+	list_t *new_node;
+	list_t *temp;
 
 	for (lens = 0; str[lens] != '\0'; lens++)
 		;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		list_t *last;
-		list_t *new_n = malloc(sizeof(list_t));
-
-		if (new_n == NULL)
-			return (NULL);
-
-		new_n->str = strdup(str);
-		new_n->next = NULL;
-
-		if (*head == NULL)
-			*head = new_n;
-		else
-		{
-			last = *head;
-			while (last->next != NULL)
-				last = last->next;
-			last->next = new_n;
-		}
-
-		return (*head);
+		free(new_node);
+		return (NULL);
 	}
+	new_node->len = lens;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new_node;
+	new_node->next = NULL;
+	return (new_node);
 }
