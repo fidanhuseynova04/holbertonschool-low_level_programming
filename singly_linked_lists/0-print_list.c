516cfb677b4f806bf47592f40ff347320be531b9
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-04-07 13:38:08 +0000

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,6 +10,20 @@
 size_t print_list(const list_t *h);
 {
 	size_t node = 0;
+	char *default_str = "[0] (nil)";
 
 	while (h)
-	
+	{
+		if (h->str == NULL)
+		{
+			printf("%s\n", default_str);
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
